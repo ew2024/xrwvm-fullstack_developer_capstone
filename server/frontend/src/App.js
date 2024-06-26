b62d0e4c876987ece1cffe0ec39730c7a3f53475
ew2024 <ewurst@san.rr.com>
2024-06-26 14:35:57 -0400

    adding temp changes to Login and Logout

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,19 @@
 import LoginPanel from "./components/Login/Login"
+//Added Register (also route path included)
+import Register from "./components/Register/Register"
+
+
+
+
+
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />}/>
+      <Route path="/test" element={<Test/>} />
     </Routes>
   );
 }

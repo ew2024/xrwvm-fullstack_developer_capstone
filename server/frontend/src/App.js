0c9a07011071613cfcdbd7abd3c5552d2fd67755
ew2024 <ewurst@san.rr.com>
2024-07-16 18:01:29 -0400

    Adding temporary changes to include posting comments

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,17 +2,21 @@ import LoginPanel from "./components/Login/Login"
 //Added Register (also route path included)
 import Register from "./components/Register/Register"
 
-
+import Dealers from './components/Dealers/Dealers';
 
 
 
 import { Routes, Route } from "react-router-dom";
-
+import Dealer from "./components/Dealers/Dealer"
+import PostReview from "./components/Dealers/PostReview"
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />}/> 
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
